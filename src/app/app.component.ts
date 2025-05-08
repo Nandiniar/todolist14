d0b304f0773ab661a15b7e0d288ecfe7a97176b3
nandini <nandani@contaque.com>
2025-05-08 14:30:21 +0530

    Add these changes
    Signed-off-by: nandini <nandani@contaque.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'todolist14';
+  task="";
+  taskList:{id:number,task:string}[]=[]; // iske 2 property hai id jiska type number hai and dusra task jiska type string hoge
+  addTask(){
+this.taskList.push({id:this.taskList.length+1,task:this.task})
+this.task=''
+console.log(this.taskList)
+  }
+deleteb(id:number){
+  this.taskList=this.taskList.filter((item)=>item.id!=id)
+}
 }
